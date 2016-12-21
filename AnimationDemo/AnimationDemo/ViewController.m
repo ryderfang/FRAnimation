abf08b0cf0c5ef70939ff5b7596d1fa5257a7b9c
fangrui <fangrui@bilibili.com>
2016-12-21 19:44:41 +0800

    layer

--- AnimationDemo/AnimationDemo/ViewController.m
+++ AnimationDemo/AnimationDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "Masonry.h"
 
-@interface ViewController ()
+@interface ViewController () <CALayerDelegate>
 
 @property (nonatomic, strong) UIView *bgView;
 
@@ -21,10 +21,29 @@
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.bgView];
     
+    CALayer *blueLayer = self.bgView.layer;
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    UIImage *image = [UIImage imageNamed:@"icon_balloon"];
+    blueLayer.contents = (__bridge id)image.CGImage;
+    blueLayer.contentsGravity = kCAGravityResizeAspect;
+    blueLayer.contentsScale = image.scale;
+//    blueLayer.masksToBounds = YES;
+//    blueLayer.contentsRect = CGRectMake(0.5, 0, 0.5, 0.5);
+//    blueLayer.contentsCenter = CGRectMake(0.25, 0.25, 0.5, 0.5);
+//    blueLayer.delegate = self;
+//    [blueLayer display];
+    blueLayer.anchorPoint = CGPointMake(1, 1);
+    
     [self.bgView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.center.equalTo(self.view);
         make.width.and.height.equalTo(@300);
     }];
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    CGContextSetLineWidth(ctx, 3.f);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextStrokeEllipseInRect(ctx, layer.bounds);
 }
 
 - (UIView *)bgView {
